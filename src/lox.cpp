182af7e3630bd485e1f6c8aefc4f735c75fe8c7b
Samir Abdallah <abdousamir8@gmail.com>
2026-04-15 00:13:33 +0200

    wip

--- src/lox.cpp
+++ src/lox.cpp
@@ -1,15 +1,12 @@
+#include "lox.hpp"
+#include <fstream>
+#include <ios>
 #include <iostream>
-#include "lox.h"
-
-void runFile(const char *fileName) {
-  std::cout << "Running : " << fileName << std::endl;
-}
-
-void runPrompt() { std::cout << "Not implemented yet, stay tuned...\n"; }
+#include <iterator>
 
 int main(int argc, char **argv) {
   if (argc > 2) {
-    std::cout << "Usage : lox_cpp [srcipt]\n";
+    std::cout << "Usage : lox_cpp [script]\n";
   } else if (argc == 2) {
     runFile(argv[1]);
   } else {
@@ -17,3 +14,12 @@ int main(int argc, char **argv) {
   }
   return 0;
 }
+void runFile(const char *fileName) {
+  std::cout << "Running : " << fileName << std::endl;
+  std::ifstream infile{fileName, std::ios_base::binary};
+  std::string content{std::istreambuf_iterator<char>{infile},
+                      std::istreambuf_iterator<char>{}};
+  run(content);
+}
+
+void runPrompt() { std::cout << "Not implemented yet, stay tuned...\n"; }
--- src/lox.hpp
+++ src/lox.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
 void runFile(const char *fileName);
 
 void runPrompt();
+
+void run(const std::string path);
